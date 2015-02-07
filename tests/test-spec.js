f0e17b0a85a5720fb28ebedac5437aa678a54750
Moran <send2moran@gmail.com>
2015-02-07 23:56:33 +0200

    add test

---
+++ tests/test-spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+var expect       = require('expect.js');
+
+
+
+describe('Post: With Spoiler', function(){
+
+
+  it('should pass', function(){
+    expect(1).to.be(1);
+  });
+
+
+});
