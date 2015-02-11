dfc1b2112b4f5607db7ea2420de6bbcab9299aa6
Moran Helman <send2moran@gmail.com>
2015-02-11 16:24:59 +0200

    add node cluster support to auto recover node

--- index.js
+++ index.js
@@ -55,12 +55,12 @@ app.get('/get/mp4', function (req, res) {
 })
 
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port)
-})
+// var server = app.listen(process.env.PORT || 3000, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log('Example app listening at http://%s:%s', host, port)
+// })
 
 
 
-//module.exports = player;
+module.exports = app;
